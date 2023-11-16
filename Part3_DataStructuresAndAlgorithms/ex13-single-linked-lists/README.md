44c39d1e8384abea7e799127d7a74d9e9b46b69c
DimsumPanda <dimsumpanda>
2023-11-16 17:23:14 -0500

    add ex13 with push, pop, shift, and unshift

---
+++ Part3_DataStructuresAndAlgorithms/ex13-single-linked-lists/README.md
@@ -0,0 +1,27 @@
+# Ex13: Single Linked Lists
+
+## Prerequisites
+If you haven't already, install venv and the requirements (e.g. pytest).
+```
+python3 -m venv venv
+. ./venv/bin/activate
+pip3 install -r requirements.txt
+```
+
+## Run Tests
+
+You can either run `pytest` which will run run all files of the form test_*.py or *_test.py in the current directory and its subdirectories. More generally, it follows standard [https://docs.pytest.org/en/7.4.x/explanation/goodpractices.html#test-discovery](test discovery rules).
+```
+pytest
+```
+or you can specify the test file.
+```
+pytest test_<file_name>.py
+```
+
+## Helpful Resources
+Some relevant resources for completing Exercise 13: Single Linked Lists:
+- pytest - https://docs.pytest.org/en/7.4.x/
+- Author's rough solution (not a full solution to get you started): https://github.com/zedshaw/learn-more-python-the-hard-way-solutions/tree/master/ex13_sllist
+- [https://docs.python.org/3/tutorial/classes.html](Python Classes Documentation)
+- [RealPython article on Writing Test](https://realpython.com/python-testing/#writing-your-first-test)
---
+++ Part3_DataStructuresAndAlgorithms/ex13-single-linked-lists/single_linked_list.py
@@ -0,0 +1,134 @@
+class SingleLinkedListNode(object):
+
+    def __init__(self, value, nxt):
+        self.value = value
+        self.next = nxt
+
+    def __repr__(self):
+        nval = self.next and self.next.value or None
+        return f"[{self.value}:{repr(nval)}]"
+
+class SingleLinkedList(object):
+    
+    def __init__(self):
+        self.begin = None
+        self.end = None
+
+    def push(self, obj):
+        """Appends a new value on the end of the list."""
+        node = SingleLinkedListNode(obj, None)
+        if self.begin == None:
+            # if it's a new list, then set this newly pushed node
+            # as the begin and end of this list
+            self.begin = node
+            self.end = self.begin
+        else:
+            self.end.next = node
+            self.end = node
+            assert self.begin != self.end
+
+        assert self.end.next == None
+
+    def pop(self):
+        """Removes the last item and returns it."""
+        if self.end == None:
+            return None
+        elif self.end == self.begin:
+            node = self.begin
+            self.end = self.begin = None
+            return node.value
+        else:
+            node = self.begin
+            # Start with the first node's "next" node
+            # This will stop at the third to last node
+            # Second to last node will be (node.next == self.end)
+            while node.next != self.end:
+                node = node.next # traverse until node = second to last node
+            assert self.end != node
+            # Save the previous end node to return
+            old_end_node_value = node.next.value
+            # Set the end to second to last node
+            self.end = node
+            # returns the original next value.
+            self.end.next = None
+            
+            return old_end_node_value
+        
+    def shift(self, obj):
+        """Another name for push but from the beginning node."""
+        node = SingleLinkedListNode(obj, None)
+        # If this is the beginning of the list
+        if self.begin == None:
+            self.begin = node
+            self.end = node
+        else:
+            # Get the self.begin and set it as the new node
+            # set the next to the previously begin node
+            node.next = self.begin
+            self.begin = node
+            assert self.begin != self.end
+
+        assert self.end.next == None
+        assert self.begin != None
+
+    def unshift(self):
+        """Removes the first item and returns it."""
+        # Set the next node of the begin node to be the begin node
+        unshift_node_value = None
+        if self.begin == None:
+            pass
+        elif self.begin.next:
+            unshift_node_value = self.begin.value
+            self.begin = self.begin.next
+        else:
+            unshift_node_value = self.begin.value
+            self.begin = self.end = None
+        return unshift_node_value
+
+    def remove(self, obj):
+        """Finds a matching item and removes it from the list."""
+
+    def first(self):
+        """Returns a *reference* to the first item, does not remove."""
+
+    def last(self):
+        """Returns a reference to the last item, does not remove."""
+
+    def count(self):
+        """Counts the number of elements in the list."""
+        node = self.begin
+        count = 0
+        while node:
+            count += 1
+            node = node.next
+        return count
+
+    def get(self, index):
+        """Get the value at index."""
+
+    def dump(self, mark):
+        """Debugging function that dumps the contents of the list."""
+
+##########################################################################
+# The following commented out lines is exploratory testing of the 
+# SingleLinkedListNode class.
+##########################################################################
+
+# listNode1 = SingleLinkedListNode("A", None)
+# print(listNode1.value)              # A
+# print(listNode1.next)               # None
+
+# listNode2 = SingleLinkedListNode("B", listNode1)
+# print(listNode2.value)              #B
+# print(listNode2.next.value)         #A
+
+# listNode3 = SingleLinkedListNode("C", listNode2)
+# print(listNode3.value)              #C
+# print(listNode3.next.value)         #B
+# print(listNode3.next.next.value)    #A
+
+# print("Print Nodes:")
+# print(listNode1)                    # [A:None]
+# print(listNode2)                    # [B:'A']
+# print(listNode3)                    # [C:'B']
+
